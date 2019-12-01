47d0e75d4b6459f31b8b5829ad1eb63047c6a384
lakkanalalitha <57029297+lakkanalalitha@users.noreply.github.com>
2019-12-01 14:58:18 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -19,8 +19,25 @@ def home():
 def predict():
 	int_features = [int(x) for x in request.form.values()]	
 	print("******************************")
-	
+	#print(int_features)
 	unseen_x = np.array(int_features)	
+	#print(unseen_x[0])
+
+	
+	import csv
+	'''
+	with open('writeToCsv.csv', 'w', newline='') as file:
+		writer = csv.writer(file)
+		writer.writerow(["family", "emotional", "physical", "financial", "job"])
+		writer.writerow([unseen_x[0], unseen_x[1], unseen_x[2], unseen_x[3], unseen_x[4]])
+	'''
+	
+	# add row to CSV file
+	with open('writeToCsv.csv', "a", newline='') as f:
+		header = ["family", "emotional", "physical", "financial", "job"]
+		writer = csv.DictWriter(f, fieldnames=header)
+		writer.writerow({'family': unseen_x[0], 'emotional': unseen_x[1], 'physical': unseen_x[2], 'financial':unseen_x[3], 'job': unseen_x[4]})
+	
 	prob = model.predict([unseen_x])
 	confidence_measure = model.predict_proba([unseen_x])[0, prob]
 	#print("Unseen Data Accuracy on K-Fold: %.3f%%" % (confidence_measure*100.0))
